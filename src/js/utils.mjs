0eeb0adddd4b3563cc105c2877afbea67bb8cfdf
jadenday20 <day18007@byui.edu>
2023-03-25 10:09:44 -0600

    added the first changes to my site

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -13,6 +13,57 @@ export function getLocalStorage(key) {
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+
+
+export function getParam(param){
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product;
+}
+
+
+// function to take a list of objects and a template and insert the objects as HTML into the DOM
+
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+) {
+  const htmlStrings = list.map(templateFn);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.insertAdjacentHTML("afterbegin", template);
+  if(callback) {
+      callback(data);
+  }
+}
+
+async function loadTemplate(path){
+  const html = await fetch(path);
+  const template = await html.text();
+  return template;
+}
+
+export async function loadHeaderFooter(){
+  const header = await loadTemplate("../partials/header.html");
+  const indexHeader = document.getElementById("main-header");
+  const footer = await loadTemplate("../partials/footer.html");
+  const indexFooter = document.getElementById("main-footer");
+  renderWithTemplate(header, indexHeader)
+  renderWithTemplate(footer, indexFooter)
+  document.getElementById("currYear").textContent = new Date().getFullYear();
+}
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
